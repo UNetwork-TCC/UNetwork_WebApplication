731bd55abd0af4104a62bea271eb279d2389d8ad
Vitor Santos <VictorHugo220BR@gmail.com>
2023-05-13 23:52:00 -0300

    Google Auth + Layout Theme + Redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
+import { CssBaseline } from '@mui/material'
+import GoogleAuth from './components/GoogleAuth'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GOOGLE_CLIENT_ID } from './constants'
 import { useState } from 'react'
+import { lightTheme } from './themes'
+import { themeContext } from './contexts'
+
 function App() {
-    const [count, setCount] = useState(0)
+    const [ theme, setTheme ] = useState(lightTheme)
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div>{count}</div>
-            <div onClick={() => setCount(count + 1)} >Counter</div>
-        </div>
+        <Router>
+            <themeContext.Provider value={{ theme, setTheme }}>
+                <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} >
+                    <CssBaseline />
+                    <Routes>
+                        <Route path="/" element={<GoogleAuth />} />
+                    </Routes>
+                </GoogleOAuthProvider>
+            </themeContext.Provider>
+        </Router>
     )
 }
 
