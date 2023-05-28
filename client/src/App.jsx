4cba7897631289106185e679f241f3ed6b84840f
Danilo Alfa <dalfahenrique@gmail.com>
2023-05-28 13:10:16 -0300

    Merge branch 'frontend' of https://github.com/UNetwork-TCC/UNetwork_WebApplication into frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,26 @@
-
 import { CssBaseline } from '@mui/material'
-import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom'
+import GoogleAuth from './components/GoogleAuth'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { GOOGLE_CLIENT_ID } from './constants'
 import { useState } from 'react'
 import { lightTheme } from './themes'
 import { themeContext } from './contexts'
-import { Home, ErrorPage } from './pages'
-import ThemeStore from './layout/ThemeStore'
-import ChatPage from './pages/Chat'
+import { Home } from './pages'
 
 function App() {
-    const [ theme, setTheme ] = useState(lightTheme)
+    const [theme, setTheme] = useState(lightTheme)
 
     return (
         <Router>
             <themeContext.Provider value={{ theme, setTheme }}>
-                <GoogleOAuthProvider clientId={ GOOGLE_CLIENT_ID }>
-                    <ThemeStore>
-                        <CssBaseline />
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/error" element={<ErrorPage />} />
-                            <Route path="/chat" element={<ChatPage />} />
-                            <Route path='*' element={<Navigate to='/error' />} />
-                        </Routes>
-                    </ThemeStore>
+                <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route path="/" element={<GoogleAuth />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/chat" element={<Chat />} />
+                    </Routes>
                 </GoogleOAuthProvider>
             </themeContext.Provider>
         </Router>
