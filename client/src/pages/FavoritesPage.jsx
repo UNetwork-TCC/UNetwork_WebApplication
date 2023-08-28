4907208cc3d38a4d05267c8474bc210447f364c0
Torugo-programer <vizombr@gmail.com>
2023-08-28 19:33:48 +0000

    Favorites Update

--- client/src/pages/FavoritesPage.jsx
+++ client/src/pages/FavoritesPage.jsx
@@ -1,23 +1,43 @@
-import { Avatar, Box, Divider, Typography, Card, Grid, Modal, Button, TextField, Checkbox, FormControlLabel, FormGroup } from '@mui/material'
+import { Avatar, Box, Divider, Typography, Card, Grid, Modal, Button, TextField, FormControl, useMediaQuery, useTheme } from '@mui/material'
 import { Add } from '@mui/icons-material'
 import Skeleton from '@mui/material/Skeleton'
 import Stack from '@mui/material/Stack'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Folder } from '../components'
 import AppLayout from '../layout/AppLayout'
-import { grey } from '@mui/material/colors'
 import CustomCheckBox from '../layout/CustomCheckBox'
 
 export default function FavoritesPage() {
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.up('md'))
+
     const [ open, setOpen ] = useState(false)
-    const ref = useRef()
-    
+
+    const [ folderAttributes, setFolderAttributes ] = useState({
+        title: '',
+        visibility: ''
+    })
+
+    const [ checkedButtons, setCheckedButtons ] = useState({
+        public: true,
+        private: false
+    })
+
     const handleOpen = () => setOpen(true)
     const handleClose = () => setOpen(false)
 
+    const createFolder = () => {
+        // ...
+
+        console.log(folderAttributes)
+
+        if (folderAttributes.visibility && folderAttributes.title)
+            handleClose()
+    }
+
     useEffect(() => {
         document.addEventListener('keydown', e => {
-            if(e.which == 27) {
+            if(e.code == 27) {
                 handleClose()  
             }
         })
@@ -25,21 +45,13 @@ export default function FavoritesPage() {
 
     return (
         <AppLayout withSidebars>
-            <Box sx={{ overflow: 'hidden' }}> 
-                <Divider sx={{ bgcolor: '#673AB7', height: '.6vh', m: '.6vh 2% 0 2%', borderRadius:'3.1vh' }} variant="middle" />
+            <Box> 
                 <Box sx={{ display: 'flex', height:'88vh' }}>
-                    <Divider sx={{ m: '10px 5px 10px 0', }} variant="middle" orientation='vertical' flexItem />
-                    <Box display={'flex'} flexDirection={'column'}></Box>
-                    <Box p={1} sx={{ mr: '5px' }}>
-                        <Box display={'flex'} sx={{ mr: '5px', cursor: 'pointer' }} onClick={handleOpen}>
-                            <Avatar sx={{ padding: '10px', margin: '10px', width: 20, height: 20, bgcolor: 'primary.main' }}>
-                                <Add sx={{ fontSize: '1em' }}/> 
-                            </Avatar>
-                            <Typography position={'relative'} top={8}>Adicionar Pasta</Typography>
-                        </Box> 
+                    <Box p={1}>
                         <Box>
                             <Card sx={{
-                                bgcolor: 'gless',
+                                background: 'linear-gradient(45deg, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.3))',
+                                boxShadow: '1px 1px 3px grey',
                                 height: '70rem',
                                 width: '65rem',
                                 display: 'flex',
@@ -47,18 +59,34 @@ export default function FavoritesPage() {
                                 justifyContent: 'start',
                                 flexDirection: 'column'
                             }}>
+                                <Box display='flex' mb={2} sx={{ cursor: 'pointer' }} onClick={handleOpen}>
+                                    <Avatar sx={{ padding: '10px', margin: '10px', width: 20, height: 20, bgcolor: 'primary.main' }}>
+                                        <Add sx={{ fontSize: '1em' }}/> 
+                                    </Avatar>
+                                    <Typography position={'relative'} top={8}>Adicionar Pasta</Typography>
+                                </Box> 
                                 <Typography variant='h4' mb={2}>Pastas</Typography>
-                                <Grid container rowSpacing={1} columnSpacing={3} gap={3} columns={4} width={'auto'}>
-                                    <Stack spacing={2} display={'flex'} alignItems={'start'}>
-                                        <Folder/>
-                                        <Skeleton variant="rounded" width={210} height={60} />
-                                        <Skeleton variant="rounded" width={210} height={60} />
-                                    </Stack> 
-                                    <Stack spacing={2} display={'flex'} alignItems={'end'}>
-                                        <Skeleton variant="rounded" width={210} height={60} />
-                                        <Skeleton variant="rounded" width={210} height={60} />
-                                        <Skeleton variant="rounded" width={210} height={60} />
-                                    </Stack>
+                                <Grid container gap={3} columns={4} width={'auto'}>
+                                    <Folder title={'pintamanhangaba'} subtitle={'memes'}/>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
+                                    <Folder></Folder>
                                 </Grid>
                             </Card>
                         </Box>
@@ -67,31 +95,54 @@ export default function FavoritesPage() {
             </Box>
             <Modal
                 open={open}
-                ref={ref}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
                 sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                 disableAutoFocus
             >
-                <Box p sx={{ height: '80vh', width: '35vw', bgcolor:'background.paper' }} borderRadius={2} >
+                <Box p sx={{ height: matches ? '45  vh' : '40vh', width: '35vw', bgcolor:'background.paper' }} borderRadius={2} >
                     <Box p={0}>
                         <Typography id="modal-modal-title" variant="h6" component="h2" m={'1rem'}>
                             Nova Pasta
                         </Typography> 
                     </Box>
                     <Box display={'flex'} flexDirection={'column'} p={2} gap={2}> 
-                        <TextField id="outline-basic" label="Título" fullWidth />
+                        <TextField 
+                            onChange={e => setFolderAttributes({ ...folderAttributes, title: e.target.value })} 
+                            id="outline-basic" 
+                            label="Título"
+                            value={folderAttributes.title}
+                            fullWidth
+                         />
                         <Typography variant='subtitle2'> Colocar visibilidade para </Typography>
-                        <FormGroup>
-                            <CustomCheckBox caption='Isso deixará sua pasta pública' title='Público'/>
-                            <CustomCheckBox caption='Isso deixará sua pasta privada' title='Privado'/>   
-                        </FormGroup>
-                        <Box display={'flex'} alignItems={'center'} justifyContent={'center'} gap={3}>
-                            <Button variant='outlined'fullWidth>
+                        <FormControl>
+                            <CustomCheckBox 
+                                onClick={
+                                    () => {
+                                        setCheckedButtons({ public: !checkedButtons.public })
+                                        setFolderAttributes({ ...folderAttributes, visibility: 'public' })
+                                    }} 
+                                checked={checkedButtons.public} 
+                                caption='Isso deixará sua pasta pública' 
+                                title='Público'
+                            />
+                            <CustomCheckBox 
+                                onClick={
+                                    () => {
+                                        setCheckedButtons({ private: !checkedButtons.private })
+                                        setFolderAttributes({ ...folderAttributes, visibility: 'private' })
+                                    }}
+                                checked={checkedButtons.private}
+                                caption='Isso deixará sua pasta privada' 
+                                title='Privado'
+                            />   
+                        </FormControl>
+                        <Box display={'flex'} alignItems={'center'} justifyContent={'center'} gap={3} >
+                            <Button onClick={handleClose} variant='outlined'fullWidth>
                                 Cancelar
                             </Button>
-                            <Button variant='outlined'fullWidth>
+                            <Button onClick={createFolder} variant='outlined'fullWidth>
                                 Criar
                             </Button>
                         </Box>
