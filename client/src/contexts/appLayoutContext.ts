8bdfd73e859a61f740ffa2ce41f3cbf729296c0c
Vitor Santos <VictorHugo220BR@gmail.com>
2023-10-05 00:43:53 -0300

    Context Update

--- client/src/contexts/appLayoutContext.ts
+++ client/src/contexts/appLayoutContext.ts
@@ -1,13 +1,15 @@
 import { createContext } from 'react'
 
 interface AppLayout {
-    leftSideBar: boolean | undefined,
-    rightSideBar: boolean | undefined,
+    leftSideBar: boolean | undefined
+    rightSideBar: boolean | undefined
+    maximizedWindow: boolean | undefined
 }
 
 export const appLayout: AppLayout = {
     leftSideBar: true,
-    rightSideBar: true
+    rightSideBar: true,
+    maximizedWindow: false
 }
 
 export const appLayoutContext = createContext(appLayout)
