16d69e2eabb6a3da46c40d6c4774183588983654
Vitor Santos <VictorHugo220BR@gmail.com>
2023-11-21 01:12:53 -0300

    User API Slice

--- client/src/features/user/index.ts
+++ client/src/features/user/index.ts
@@ -1,30 +1,53 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import * as api from '$api'
+import { createSlice } from '@reduxjs/toolkit'
+import { apiSlice } from '$api'
 import { type User } from '$types'
 
 const initialState: { user: User | Record<string, unknown> } = {
     user: {}
 }
 
-export const getUser = createAsyncThunk(
-    'user/get',
-    async (id: string) => {
-        const { data } = await api.getUser(id)
-
-        return data
-    }
-)
-
 export const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {},
-    extraReducers: builder => {
-        builder.addCase(getUser.fulfilled, (state, action) => {
-            state.user = action.payload
-        })
-    }
+    reducers: {}
 })
 
-// export const { fetchNews } = newsSlice.actions
+// User API Slice
+
+const userApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        fetchUsers: builder.mutation<User, unknown>({
+            query: () => '/user'
+        }),
+
+        getUser: builder.mutation<User, string>({
+            query: id => `/user/${id}`
+        }),
+
+        updateUser: builder.mutation<User, Partial<User>>({
+            query: data => ({
+                url: '/user',
+                method: 'PATCH',
+                body: data
+            })
+        }),
+
+        deleteUser: builder.mutation<User, string>({
+            query: id => ({
+                url: `/user/${id}`,
+                method: 'DELETE'
+            })
+        })
+    })
+})
+
+// Exports
+
+export const {
+    useDeleteUserMutation,
+    useFetchUsersMutation,
+    useGetUserMutation,
+    useUpdateUserMutation
+} = userApiSlice
+
 export const userReducer = userSlice.reducer
