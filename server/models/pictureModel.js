1b066236d91a125c10e1e1e1d51d0badfa7754f0
Vitor Hugo Rodrigues dos Santos <71114564+VicktzZ@users.noreply.github.com>
2023-11-23 15:04:11 -0300

    Update pictureModel.js

--- server/models/pictureModel.js
+++ server/models/pictureModel.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose'
 const pictureSchema = new mongoose.Schema({
     name: { type: String, required: true },
     src: { type: String, required: true },
+    userId: { type: String, required: true },
+    at: {
+        id: { type: String, required: true },
+        type: { type: String, required: true }
+    } 
 })
 
 const pictureModel = mongoose.model('Pictures', pictureSchema, 'Pictures')
