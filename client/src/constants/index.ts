c056b78e136ec5abdf16d417dd7021b3d9f80dbd
Vitor Santos <VictorHugo220BR@gmail.com>
2023-11-23 21:17:04 -0300

    API Adress Change

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_API_KEY: string = 'AIzaSyCOLDSLmmBjEDIlOmzHOEoAakcZQRlp31Y'
 export const GOOGLE_CLIENT_ID: string = '352226282642-jokokdl4q6ccrhrfgbhgj6pb4jqupo54.apps.googleusercontent.com'
 export const GOOGLE_SECRET_KEY: string = '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'
-export const API_BASE_URL: string = 'https://musical-space-telegram-q65vp9vxgv43xpr7-3000.app.github.dev' 
+export const API_BASE_URL: string = 'https://unetwork-api.onrender.com/' 
 export const REDUX_PERSIST_STORE_KEY = ')7#o6ub43kJ1@8phr2kuICLYI75s4/£[5NeHxbBe"~N;Q(GDZv'
 export const GET_IMAGE = (imgId: string): string => { return  API_BASE_URL + '/' + imgId }
